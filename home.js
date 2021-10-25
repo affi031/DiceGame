153a0dadb9cb8d9b12298cfe5d46976c40b93f5f
affi031 <afnanshaikh98@gmail.com>
2021-10-25 12:10:07 +0530

    Dice Game

---
+++ home.js
@@ -0,0 +1,22 @@
+var randonNumber1 =  Math.floor(Math.random() * 6) +1;
+var randomDiceImage1 = "dice"+randonNumber1+".png"; //  dice1.png
+var randomSourceImage1 = "assets/"+randomDiceImage1; //  assets/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomSourceImage1);
+
+
+var randonNumber2 =  Math.floor(Math.random() * 6) +1;
+var randomDiceImage2 = "dice"+randonNumber2+".png"; // dice1.png
+var randomSourceImage2 = "assets/"+randomDiceImage2;  // assets/dice6.png
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomSourceImage2);
+
+if(randonNumber1 > randonNumber2){
+    document.querySelector("h1").innerHTML = "😎 Player 1 Wins!";
+}
+else if  (randonNumber1 < randonNumber2){
+    document.querySelector("h1").innerHTML = "😎 Player 2 Wins!"
+}
+else{
+    document.querySelector("h1").innerHTML = "😒 Draw"
+}
